7f19a8077ea7306bae32d530c8d434b4be7d4abc
KaungZawHein114 <kaungzawhein114@gmail.com>
2025-08-28 10:44:30 +0630

    port changes

--- main.py
+++ main.py
@@ -196,4 +196,6 @@ def random_tip():
     return jsonify(generate_random_tip())
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
